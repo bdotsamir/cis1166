76c267cb547c42952c3d9239df0ae1512b668c78
bdotsamir <samir.js.zip@gmail.com>
2023-10-23 16:01:48 -0400

    matricies: implement sum method

---
+++ matricies/matricies.ts
@@ -0,0 +1,58 @@
+export type TwoDimensionalT<T> = T[][]
+
+export default class Matrix<T> {
+
+  public readonly numRows: number;
+  public readonly numColumns: number;
+  
+  private readonly matrix: TwoDimensionalT<T> = [];
+
+  constructor(array: TwoDimensionalT<T>) {
+    this.numRows = array.length;
+    this.numColumns = array[0].length;
+
+    this.matrix = array;
+  }
+
+  public sum(otherMatrix: Matrix<number>): Matrix<number> {
+    if (this.numRows !== otherMatrix.numRows
+      || this.numColumns !== otherMatrix.numColumns) {
+      throw new RangeError("Number of rows and columns must be equal");
+    }
+
+    if (typeof this.matrix[0][0] !== 'number') {
+      throw new TypeError("Matrix must be of type `number` to sum.");
+    }
+
+    const summedRawMat: TwoDimensionalT<number> = [];
+    for (let row = 0; row < this.matrix.length; row++) {
+      for (let column = 0; column < this.matrix[row].length; column++) {
+        // At this point we've verified that the matrix is a matrix of numbers
+        const thisElement = this.at(row, column) as number;
+        const otherElement = otherMatrix.at(row, column);
+        summedRawMat[row][column] = thisElement + otherElement;
+      }
+    }
+
+    return new Matrix<number>(summedRawMat);
+  }
+
+  public at(row: number, column: number): T {
+    return this.matrix[row][column];
+  }
+
+  public *rowIterator(): IterableIterator<T[]> {
+    for (const row of this.matrix) {
+      yield row;
+    }
+  }
+
+  public *elementIterator(): IterableIterator<T> {
+    for (const row of this.rowIterator()) {
+      for (const element of row) {
+        yield element;
+      }
+    }
+  }
+
+}
