faac88e0205db02a3b13c562bd29c36b42f7ef3e
bdotsamir <samir.js.zip@gmail.com>
2023-12-01 23:11:51 -0500

    specialmatrix: add composed & transposed matrices, then multiply them.

--- homography/video/specialmatrix.py
+++ homography/video/specialmatrix.py
@@ -11,7 +11,7 @@ class SpecialMatrix(Scene):
     self.play(Write(special_matrix))
     self.wait(1)
 
-class ShortMatrix(Scene):
+class BigMatrix(Scene):
   def construct(self):
     source_pair_coords = [
       "(0, 0)",
@@ -58,7 +58,70 @@ class ShortMatrix(Scene):
     )
     self.remove(d_coord_group)
 
-    self.wait(10)
+    self.wait(5)
+
+    composed_xy_matrix = Matrix([
+      ["x_{s}^{(1)}", "y_{s}^{(1)}", "1", "0", "0", "0", "- x_{d}^{(1)}x_{s}^{(1)}", "-x_{d}^{(1)}y_{d}^{(1)}", "-x_{d}^{(1)}"],
+      [0, 0, 0, "x_{s}^{(1)}", "y_{s}^{(1)}", 1, "-y_{d}^{(1)}x_{s}^{(1)}", "-y_{d}^{(1)}y_{s}^{(1)}", "-y_{d}^{(1)}"],
+      [r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots"],
+      ["x_{s}^{(i)}", "y_{s}^{(i)}", "1", "0", "0", "0", "- x_{d}^{(i)}x_{s}^{(i)}", "-x_{d}^{(i)}y_{d}^{(i)}", "-x_{d}^{(i)}"],
+      [0, 0, 0, "x_{s}^{(i)}", "y_{s}^{(i)}", 1, "-y_{d}^{(i)}x_{s}^{(i)}", "-y_{d}^{(i)}y_{s}^{(i)}", "-y_{d}^{(i)}"],
+      [r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots", r"\vdots"],
+      ["x_{s}^{(n)}", "y_{s}^{(n)}", "1", "0", "0", "0", "- x_{d}^{(n)}x_{s}^{(n)}", "-x_{d}^{(n)}y_{d}^{(n)}", "-x_{d}^{(n)}"],
+      [0, 0, 0, "x_{s}^{(n)}", "y_{s}^{(n)}", 1, "-y_{d}^{(n)}x_{s}^{(n)}", "-y_{d}^{(n)}y_{s}^{(n)}", "-y_{d}^{(n)}"],
+    ], h_buff=1.5, element_alignment_corner=DOWN)
+
+    self.play(Transform(special_matrix, composed_xy_matrix))
+    self.play(special_matrix.animate.shift(UP * 0.5))
+    c_mat_brace = Brace(special_matrix).set_color(YELLOW)
+    c_mat_brace.next_to(special_matrix, DOWN, buff=0.05)
+    text_A = MathTex(r"A").set_color(YELLOW)
+    t_A_copy = text_A.copy()
+    text_A.next_to(c_mat_brace, DOWN, buff=0.1)
+
+    self.play(GrowFromCenter(c_mat_brace), FadeIn(text_A))
+    
+    self.wait(2)
+
+    transposed_composed_xy_matrix = Matrix([
+      ["x_{s}^{(1)}", 0, r"\dots", "x_{s}^{(i)}", 0, r"\dots", "x_{s}^{(n)}", 0],
+      ["y_{s}^{(1)}", 0, r"\dots", "y_{s}^{(i)}", 0, r"\dots", "y_{s}^{(n)}", 0],
+      [1, 0, r"\dots", 1, 0, r"\dots", 1, 0],
+      [0, "x_{s}^{(1)}", r"\dots", 0, "x_{s}^{(i)}", r"\dots", 0, "x_{s}^{(n)}"],
+      [0, "y_{s}^{(1)}", r"\dots", 0, "y_{s}^{(i)}", r"\dots", 0, "y_{s}^{(n)}"],
+      [0, 1, r"\dots", 0, 1, r"\dots", 0, 1],
+      ["-x_{d}^{(1)}x_{s}^{(1)}", "-y_{d}^{(1)}x_{s}^{(1)}", r"\dots", "-x_{d}^{(i)}x_{s}^{(i)}", "-y_{d}^{(i)}x_{s}^{(i)}", r"\dots", "-x_{d}^{(n)}x_{s}^{(n)}", "-y_{d}^{(n)}x_{s}^{(n)}",],
+      ["-x_{d}^{(1)}y_{s}^{(1)}", "-y_{d}^{(1)}y_{s}^{(1)}", r"\dots", "-x_{d}^{(i)}y_{s}^{(i)}", "-y_{d}^{(i)}y_{s}^{(i)}", r"\dots", "-x_{d}^{(n)}y_{s}^{(n)}", "-y_{d}^{(n)}y_{s}^{(n)}",],
+      ["-x_{d}^{(1)}", "-y_{d}^{(1)}", r"\dots", "-x_{d}^{(i)}", "-y_{d}^{(i)}", r"\dots", "-x_{d}^{(n)}", "-y_{d}^{(n)}",]
+    ], h_buff=2.1,element_alignment_corner=DOWN)
+
+    transposed_composed_xy_matrix.scale(0.8)
+    transposed_composed_xy_matrix.shift(UP * 0.3)
+
+    text_AT = MathTex("A^{T}").set_color(YELLOW)
+    text_AT.next_to(c_mat_brace, DOWN, buff=0.1),
+
+    self.play(
+      Transform(special_matrix, transposed_composed_xy_matrix),
+      ReplacementTransform(text_A, text_AT),
+      c_mat_brace.animate.next_to(special_matrix, DOWN, buff=0.05)
+    )
+    self.wait(3)
+
+    self.play(FadeOut(special_matrix, c_mat_brace))
+
+    mult_group = VGroup(
+      text_AT.copy(),
+      t_A_copy
+    ).arrange()
+
+    self.play(
+      text_AT.animate.center().become(mult_group)
+    )
+    self.wait(3)
+
+    self.play(FadeOut(text_AT))
+    self.wait(1)
 
 class LongAndComplicated(Scene):
   def construct(self):
